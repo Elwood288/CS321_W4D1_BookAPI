39461d1f85f241c10a29850c70fe97dbd53ad7b8
Elwood288 <elwood288@gmail.com>
2019-08-19 18:10:28 -0500

    Homework Done

--- CS321_W4D1_BookAPI/Controllers/BooksController.cs
+++ CS321_W4D1_BookAPI/Controllers/BooksController.cs
@@ -47,7 +47,7 @@ namespace CS321_W4D1_BookAPI.Controllers
             {
                 // TODO: convert apimodel to domain model
                 // add the new book
-                _bookService.Add(newBook);
+                _bookService.Add(newBook.ToDomainModel());
             }
             catch (System.Exception ex)
             {
@@ -62,7 +62,7 @@ namespace CS321_W4D1_BookAPI.Controllers
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] BookModel updatedBook)
         {
-            var book = _bookService.Update(updatedBook);
+            var book = _bookService.Update(updatedBook.ToDomainModel());
             if (book == null) return NotFound();
             return Ok(book.ToApiModel());
         }
@@ -75,6 +75,26 @@ namespace CS321_W4D1_BookAPI.Controllers
             if (book == null) return NotFound();
             _bookService.Remove(book);
             return NoContent();
+        }
+
+        [HttpGet("/api/authors/{authorId}/books")]
+        public IActionResult GetBooksForAuthor(int authorId)
+        {
+            var bookModels = _bookService
+                .GetBooksForAuthor(authorId)
+                .ToApiModels();
+
+            return Ok(bookModels);
+        }
+
+        [HttpGet("/api/publishers/{publisherId}/books")]
+        public IActionResult GetBooksForPublisher(int publisherId)
+        {
+            var bookModels = _bookService
+                .GetBooksForAuthor(publisherId)
+                .ToApiModels();
+
+            return Ok(bookModels);
         }
     }
 }
---
+++ CS321_W4D1_BookAPI/Services/PublisherService.cs
@@ -0,0 +1,73 @@
+﻿using CS321_W4D1_BookAPI.Data;
+using CS321_W4D1_BookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS321_W4D1_BookAPI.Services
+{
+    public class PublisherService : IPublisherService
+    {
+
+        private readonly BookContext _bookContext;
+
+        public PublisherService(BookContext bookContext)
+        {
+           
+            _bookContext = bookContext;
+        }
+
+        public Publisher Add(Publisher publisher)
+        {
+            // TODO: implement add
+            _bookContext.Publisher.Add(publisher);
+            _bookContext.SaveChanges();
+            return publisher;
+        }
+
+        public Publisher Get(int id)
+        {
+            // TODO: return the specified Publisher using Find()
+            return _bookContext.Publisher.Find(id);
+        }
+
+        public IEnumerable<Publisher> GetAll()
+        {
+            // TODO: return all Publishers using ToList()
+            return _bookContext.Publisher.ToList();
+        }
+
+        public Publisher Update(Publisher updatedPublisher)
+        {
+            // get the ToDo object in the current list with this id 
+            var currentPublisher = _bookContext.Publisher.Find(updatedPublisher.Id);
+
+            // return null if todo to update isn't found
+            if (currentPublisher == null) return null;
+
+            // NOTE: This method is already completed for you, but note
+            // how the property values are copied below.
+
+            // copy the property values from the changed todo into the
+            // one in the db. NOTE that this is much simpler than individually
+            // copying each property.
+            _bookContext.Entry(currentPublisher)
+                .CurrentValues
+                .SetValues(updatedPublisher);
+
+            // update the todo and save
+            _bookContext.Publisher.Update(currentPublisher);
+            _bookContext.SaveChanges();
+            return currentPublisher;
+        }
+
+        public void Remove(Publisher publisher)
+        {
+            // TODO: remove the Publisher
+            _bookContext.Publisher.Remove(publisher);
+            _bookContext.SaveChanges();
+        }
+
+    }
+}
